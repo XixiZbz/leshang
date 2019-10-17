65a80993ecf84ab42ef6ace7f43f7c924c01871b
zbz <zbz26678325@163.com>
2019-10-17 23:33:28 +0800

    update

--- test.py
+++ test.py
@@ -11,5 +11,8 @@ from selenium import webdriver
 browser = webdriver.Ie()
 url = "https://login.taobao.com/member/login.jhtml?style=mini&newMini2=true&from=alimama&redirectURL=http%3A%2F%2Flogin.taobao.com%2Fmember%2Ftaobaoke%2Flogin.htm%3Fis_login%3d1&full_redirect=true"
 browser.get(url)
+button = browser.find_element_by_xpath("//button[class='J_Submit']")
+print(button)
+button.click()
 cookies = browser.get_cookies()
 print(cookies)
