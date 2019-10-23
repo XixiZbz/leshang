ab1456844dee59e25e745ee2fec785f237d78576
zbz <zbz26678325@163.com>
2019-10-23 23:00:04 +0800

    update

--- test.py
+++ test.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 
 from selenium import webdriver
-
+import requests
 browser = webdriver.Ie()
 url = "https://login.taobao.com/member/login.jhtml?style=mini&newMini2=true&from=alimama&redirectURL=http%3A%2F%2Flogin.taobao.com%2Fmember%2Ftaobaoke%2Flogin.htm%3Fis_login%3d1&full_redirect=true"
 browser.get(url)
@@ -15,8 +15,19 @@ button = browser.find_element_by_xpath("//button[contains(text(),'登 录')]")
 print(button)
 button.click()
 cookies = browser.get_cookies()
-print(cookies)
 browser.close()
-#
-# with open("./cookies.txt",'w') as f:
-#     f.write(cookies)
+
+format_cookies_list = eval(cookies)
+format_cookies = {x["name"]:x["value"] for x in cookies}
+
+
+
+# with open("./cookies",'r') as f:
+#     cookies = f.read()
+
+tb_token= format_cookies[' _tb_token_']
+s = requests.session()
+s.cookies.update(cookies)
+url = "https://pub.alimama.com/report/getCPPaymentDetails.json?t=1571668389426&_tb_token_={tb_token}&startTime=2019-10-15&endTime=2019-10-21&payStatus=&queryType=1&toPage=1&perPageSize=40&positionIndex=&jumpType=0".format(tb_token)
+res = s.get(url)
+print(res.text)
