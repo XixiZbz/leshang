dedc54e3a0abc5f0ed96c2312f890b6774aec88f
zbz <zbz26678325@163.com>
2019-10-23 23:14:07 +0800

    update

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ format_cookies = {x["name"]:x["value"] for x in cookies}
 # with open("./cookies",'r') as f:
 #     cookies = f.read()
 
-tb_token= format_cookies[' _tb_token_']
+tb_token= format_cookies['_tb_token_']
 s = requests.session()
 s.cookies.update(cookies)
 url = "https://pub.alimama.com/report/getCPPaymentDetails.json?t=1571668389426&_tb_token_={tb_token}&startTime=2019-10-15&endTime=2019-10-21&payStatus=&queryType=1&toPage=1&perPageSize=40&positionIndex=&jumpType=0".format(tb_token)
